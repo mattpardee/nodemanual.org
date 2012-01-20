535b1ea75e3af44608206a2e8e50ff7574d91e2c
Garen Torikian <gjtorikian@gmail.com>
2012-01-20 10:39:07 -0800

    Remove erroneous node_modules dir

--- node_modules/creationix/index.js
+++
@@ -1,9 +0,0 @@
-require('fs').readdirSync(__dirname).forEach(function (name) {
-  if (name !== 'index.js' && name.substr(name.length-3) === ".js") {
-    var name = name.substr(0, name.length - 3);
-    Object.defineProperty(exports, name, {get: function () {
-      return require("./" + name);
-    }, enumerable: true});
-  }
-});
-
--- node_modules/fs-tools/doc/javascripts/pdoc/item_index.js
+++
@@ -1,8 +0,0 @@
-if (!window.PDoc) window.PDoc = {};
-PDoc.elements = {
-  'FsTools': { 'name': 'FsTools', 'type': 'namespace', 'path': 'FsTools/index.html' },
-'FsTools.walk': { 'name': 'FsTools.walk', 'type': 'class method', 'path': 'FsTools/walk/index.html' },
-'FsTools.remove': { 'name': 'FsTools.remove', 'type': 'class method', 'path': 'FsTools/remove/index.html' },
-'FsTools.mkdir': { 'name': 'FsTools.mkdir', 'type': 'class method', 'path': 'FsTools/mkdir/index.html' },
-'FsTools.copy': { 'name': 'FsTools.copy', 'type': 'class method', 'path': 'FsTools/copy/index.html' }
-};
