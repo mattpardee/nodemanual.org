e6316fe71736afb1e42d9fd29429adee21e1ca7c
Garen Torikian <gjtorikian@gmail.com>
2012-01-22 02:49:43 -0800

    Split out title & byline; fix horrible spelling errors

--- src/latest/nodejs_ref_guide/index.js
+++ src/latest/nodejs_ref_guide/index.js
@@ -4,7 +4,7 @@
  *
  * Node.js is a server-side Javascript environment. It's event-driven, asynchronous, and allows you to write a web server in a relatively quick amount of time.
  *
- * This section is the Node.js Reference API in its entirity. The origin of this content was from the original Nodejs.org documentation, with some heavy edits and improvements.
+ * This section is the Node.js Reference API in its entirety. The origin of this content was from the original Nodejs.org documentation, with some heavy edits and improvements.
  * 
  * In addition, most samples can be run live and online by importing them into your [Cloud 9 IDE](http://c9.io) account.
  *
