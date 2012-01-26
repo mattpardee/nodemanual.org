13bda306b23d71c1140897e24d7542e264edd02e
Garen Torikian <gjtorikian@gmail.com>
2012-01-25 22:15:35 -0800

    Optimize build.js; remove duplicate event from tls

--- build.js
+++ build.js
@@ -6,55 +6,69 @@ var argv = require("optimist").argv,
     fs = require('fs'),
     md2html = require('marked'),
     jade = require('jade');
-    
+
 var version = argv._[0];
 var versions = [];
+var jadeTemplateFile = "resources/landing/layout.jade";
+var jadeTemplate = fs.readFileSync(jadeTemplateFile);
+
+//var outDir = "out/nodejs_dev_guide";
 
 console.log("GENERATING DOCUMENTATION".green);
 
-fs.readdir("./src", function(err, files) {
+var util = require('util'),
+    exec = require('child_process').exec,
+    child;
+
+ls = exec('ls ./src/');
+ls.stdout.on('data', function(data) {
     if ("latest" == version) {
         versions.push("latest");
     }
-    else if (/[\d]\.[\d]\.[\d]/.test(version)) {         
+    else if (/[\d]\.[\d]\.[\d]/.test(version)) {
         versions.push("v" + version);
     }
     else {
-        versions = files;
-        versions = versions.filter(function (value) {
-            return (value === '' || value == "index.md") ? 0 : 1;
+        data = data.replace(/^\.$/, "").replace(/index\.md/, "");
+
+        versions = data.split("\n");
+        versions = versions.filter(function(value) {
+            return (value === '') ? 0 : 1;
         });
     }
-    
-    makeManualDocs(versions);
-    makeJSRefDocs(versions);
-    makeNodeJSRefDocs(versions);        
+
+    versions.forEach(function(element, index, array) {
+        makeManualDocs(element);
+        makeJSRefDocs(element);
+    });
 });
 
+function makeManualDocs(element) {
 
-function makeManualDocs(versions) {
-    versions.forEach(function (element, index, array) {
-        var outDir = "out/" + element;
-        var tmpDir = "tmp/" + element;
+    var outDir = "out/" + element;
+    var tmpDir = "tmp/" + element;
 
-        docs.clean(tmpDir + "/nodejs_dev_guide");
-        docs.clean(outDir + "/nodejs_dev_guide");
+    docs.clean(tmpDir + "/nodejs_dev_guide");
+    docs.clean(outDir + "/nodejs_dev_guide");
 
-        docs.copyassets(outDir, "nodejs_dev_guide");
-        docs.generate(outDir + "/nodejs_dev_guide", process.cwd() + "/src/" + element + "/nodejs_dev_guide/", "nodejs_dev_guide");
+    docs.copyassets(outDir, "nodejs_dev_guide");
+    docs.generate(outDir + "/nodejs_dev_guide", process.cwd() + "/src/" + element + "/nodejs_dev_guide/", "nodejs_dev_guide");
 
-        makeIndexes(element);
-    });    
+    makeIndexes(element);
+
+    //makeJSRefDocs(versions);
 }
 
-function makeIndexes(version) {
-    var jadeTemplateFile = "resources/landing/layout.jade";
-    var jadeTemplate = fs.readFileSync(jadeTemplateFile);
+function makeIndexes(element) {
+    var readContentStream = fs.createReadStream("src/index.md", {
+        encoding: 'utf8'
+    });
 
-    var readContentStream = fs.createReadStream("src/index.md", {encoding: 'utf8'});
-
-    readContentStream.on('data', function (data) {
-        var fn = jade.compile(jadeTemplate, {filename: jadeTemplateFile, pretty: false});
+    readContentStream.on('data', function(data) {
+        var fn = jade.compile(jadeTemplate, {
+            filename: jadeTemplateFile,
+            pretty: false
+        });
 
         var dataArray = data.split("\n");
 
@@ -64,53 +78,49 @@ function makeIndexes(version) {
         var vars = extend({
             title: title,
             data: data,
-            whoAmI: version,
+            whoAmI: element,
             markdown: markdown
         });
 
         var r = fn(vars);
-        
-        var writeStream = fs.createWriteStream("out/" + version + "/index.html", {flags: "w"});
-                
+
+        var writeStream = fs.createWriteStream("out/" + element + "/index.html", {
+            flags: "w"
+        });
+
         writeStream.write(r);
-    });    
-}
-
-function makeJSRefDocs(versions) {
-    versions.forEach(function (element, index, array) {
-        ndoc.main(["--path=./src/" + element + "/js_doc", "-o", "./out/" + element + "/js_doc/", "-t", "Node.js Reference", "--skin", "./resources/nodejs_ref_guide/skins"], function(err) {
-            if (err) {
-                console.error(err);
-                process.exit(-1);
-            }
-
-            if (index == array.length - 1)
-            {
-                console.log("All Done!".green);
-                process.exit(0);
-            }
-
-        });
     });
 }
 
-function makeNodeJSRefDocs(versions) {
-    versions.forEach(function (element, index, array) {
-        ndoc.main(["--path=./src/" + element + "/nodejs_ref_guide", "-o", "./out/" + element + "/nodejs_ref_guide/", "-t", "Node.js Reference", "--skin", "./resources/nodejs_ref_guide/skins"], function(err) {
-            if (err) {
-                console.error(err);
-                process.exit(-1);
-            }
-        });
+function makeJSRefDocs(element) {
+    ndoc.main(["--path=./src/" + element + "/js_doc", "-o", "./out/" + element + "/js_doc/", "-t", "Node.js Reference", "--skin", "./resources/nodejs_ref_guide/skins"], function(err) {
+        if (err) {
+            console.error(err);
+            process.exit(-1);
+        }
+
+        makeNodeJSRefDocs(element);
     });
 }
+
+function makeNodeJSRefDocs(element) {
+    ndoc.main(["--path=./src/" + element + "/nodejs_ref_guide", "-o", "./out/" + element + "/nodejs_ref_guide/", "-t", "Node.js Reference", "--skin", "./resources/nodejs_ref_guide/skins"], function(err) {
+        if (err) {
+            console.error(err);
+            process.exit(-1);
+        }
+    });
+}
+
+// helpers
 
 function markdown(text) {
     return md2html(text);
 }
 
 function extend(o, plus) {
-    var r = {}, i;
+    var r = {},
+        i;
     for (i in o) {
         if (o.hasOwnProperty(i)) {
             r[i] = o[i];
--- src/0.6.7/nodejs_ref_guide/Modules/tls/tls.CleartextStream.js
+++ src/0.6.7/nodejs_ref_guide/Modules/tls/tls.CleartextStream.js
@@ -17,14 +17,6 @@
 **/ 
 
 /**
- * tls.CleartextStream@secureConnect()
- *
- * This event is emitted after a new connection has been successfully handshaked. The listener will be called no matter if the server's certificate was authorized or not. 
- *
- * It is up to the user to test `cleartextStream.authorized` to see if the server certificate was signed by one of the specified CAs. If `cleartextStream.authorized === false`, then the error can be found in `cleartextStream.authorizationError`. Also if NPN was used, you can check `cleartextStream.npnProtocol` for the negotiated protocol.
-**/ 
-
-/**
  * tls.CleartextStream@clientError(exception)
  * - exception (Error): The standard Error object
  * 
