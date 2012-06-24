2e590900d66fdd4cd005ca25fd42b5fb8f854065
Garen Torikian <gjtorikian@gmail.com>
2012-06-23 22:54:50 -0700

    Update JSON

--- ast_build.js
+++ ast_build.js
@@ -1,6 +1,6 @@
 var panino = require("panino");
     
-panino.main(["--path=./src/latest/nodejs_ref_guide", "-s", "-d", "-e", "markdown", "-g", "javascript", "-f", "ast", "-p", "./parseOptions.json", "-a", "./additionalObjs.json", "-o", "./nodemanual.input.json"], function(err) {
+panino.main(["--path=./src/latest/nodejs_ref_guide", "-s", "-d", "-e", "markdown", "-g", "javascript", "-f", "ast", "-p", "./parseOptions.json", "-a", "./additionalObjs.json", "-o", "./nodemanual.input"], function(err) {
     if (err) {
         console.error(err);
         process.exit(-1);
