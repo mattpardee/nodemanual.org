8da563992c2aed887af90f02bfda07e6dff9ff55
Garen Torikian <gjtorikian@gmail.com>
2012-09-01 10:13:01 -0700

    Update json build

--- ast_build.js
+++
@@ -1,8 +0,0 @@
-var panino = require("panino");
-    
-panino.main(["--path=./src/latest/nodejs_ref_guide", "-s", "-d", "-e", "markdown", "-g", "javascript", "-f", "ast", "-p", "./parseOptions.json", "-a", "./additionalObjs.json", "-o", "./nodemanual.input"], function(err) {
-    if (err) {
-        console.error(err);
-        process.exit(-1);
-    }
-});
